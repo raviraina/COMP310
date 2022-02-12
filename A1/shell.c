869ae0a403e6c75f8724552173744704407f6cf6
Sarvasv Arora <sarvasvarora@gmail.com>
2022-02-12 15:46:35 -0500

    command chain implementation

--- A1/shell.c
+++ A1/shell.c
@@ -47,91 +47,41 @@ int main(int argc, char *argv[])
 // Extract words from the input then call interpreter
 int parseInput(char ui[])
 {
-
-	char tmp[200];
-	char *words[100];
-	int a, b;
+	char *words[100]; // holds parsed commands and arguments until they are sent to the interpreter
 	int w = 0; // wordID -- number of words in the user input (in each command)
-	int c = 0; // commandID -- number of commands in the user input
-	int return_val = 0;
-	const char delim[2] = ";";
-	char *token;
-	char *ptr = ui;
 
-	token = strtok(ui, delim);
+	// tokens and checkpoints for strtok_r
+	char* cmd_token;
+	char* cmd_token_chkpt = ui;
+	char* word_token;
+	char* word_token_chkpt;
 
-	while (token != NULL)
+	// delimeters for parsing multiple commands
+	const char* delim = strdup(";");
+	const char* word_sep = strdup(" ");
+
+	int return_val = 0; // return value of the command(s)
+
+	// parse user input (ui) into separate commands separated by semi-colons (;)
+	while ((cmd_token = strtok_r(cmd_token_chkpt, delim, &cmd_token_chkpt)) != NULL)
 	{
-		if(strcmp(token, "\0") == 0) continue;
-        printf("token : %s\n", token);
-		for (a = 0, w = 0; token[a] == ' ' && a < MAX_USER_INPUT; a++); // skip leading white spaces
-		while (token[a] != '\0' && a < MAX_USER_INPUT)
-		{
-
-			for (b = 0; token[a] != '\0' && token[a] != ' ' && a < MAX_USER_INPUT; a++, b++)
-				tmp[b] = token[a]; // extract a word
-
-			tmp[b] = '\0';
-
-			words[w] = strdup(tmp);
-
-			a++;
-			w++;
+		if(strcmp(cmd_token, "\0") == 0 || strcmp(cmd_token, " ") == 0 || strcmp(cmd_token, "") == 0) { 
+			strtok(NULL, delim);
+			continue;
 		}
 
-		// for(int i = 0; i < w; i++) {
-		// 	printf("%s\n", words[i]);
-		// }
+		// parse command name and arguments
+		word_token_chkpt = cmd_token;
+		while ((word_token = strtok_r(word_token_chkpt, word_sep, &word_token_chkpt)) != NULL)
+		{
+			words[w++] = strdup(word_token);
+		}
 
+		// call the interpreter
 		return_val = interpreter(words, w);
-		if (return_val)
-			return return_val;
-
-		token = strtok(NULL, delim);
-		// ptr = NULL;
+		if (return_val) return return_val; // return if there is an error (return_val != 0)
+		
+		w = 0;// reset wordID for the following command
 	}
 	return return_val;
-
-	// for(a=0; ui[a]==' ' && a<MAX_USER_INPUT; a++);		// skip leading white spaces
-
-	// while(ui[a] != '\0' && a<MAX_USER_INPUT) {
-
-	// 	for(b=0; ui[a]!='\0' && ui[a]!=' ' && a<MAX_USER_INPUT; a++, b++)
-	// 		tmp[b] = ui[a];						// extract a word
-
-	// 	tmp[b] = '\0';
-
-	// 	words[w] = strdup(tmp);
-
-	// 	a++;
-	// 	w++;
-	// }
-
-	// while(ui[a] != '\0' && a<MAX_USER_INPUT) {
-
-	// 	for(b=0; ui[a]!='\0' && ui[a]!=' ' && ui[a] != ';' && a<MAX_USER_INPUT; a++, b++)
-	// 		tmp[b] = ui[a];						// extract a word
-
-	// 	tmp[b] = '\0';
-
-	// 	words[w] = strdup(tmp);
-
-	// 	w++;
-
-	// 	if(ui[a] == ';') {
-
-	// 		printf("w is : %d\n", w);
-	// 		for (int i = w; i < 100; i++) words[i] = strdup("\0");
-
-	// 		for(int i=0; i < w; i++) printf("%s -- ", words[i]);
-	// 		return_val = interpreter(words, w);
-	// 		if(return_val) return return_val;
-	// 		w = 0;
-	// 	}
-
-	// 	a++;
-	// }
-	// return return_val;
-
-	// return interpreter(words, w);
 }
