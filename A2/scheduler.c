e8989e1e2c9731feaf3bef96c5d8c95f91efa852
Sarvasv Arora <sarvasvarora@gmail.com>
2022-03-04 14:21:00 -0500

    schedulers init

---
+++ A2/scheduler.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pcb.h"
+#include "readyqueue.h"
+
+
+// Runs the processes in the ready queue according to FCFS scheduling policy
+int FCFS_scheduler(rq_t *rq) {
+    
+}
+
+
+// Runs the processes in the ready queue according to SJF scheduling policy
+int SJF_scheduler() {
+    return 0;
+}
+
+
+// Runs the processes in the ready queue according to RR scheduling policy
+int RR_scheduler() {
+    return 0;
+}
+
+
+// Runs the processes in the ready queue according to AGING scheduling policy
+int AGING_scheduler() {
+    return 0;
+}
