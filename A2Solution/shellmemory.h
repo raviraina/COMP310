7f183af9127d13d3aa2f35ef9b426923b80ad0ac
Sarvasv Arora <sarvasvarora@gmail.com>
2022-04-01 19:32:24 -0400

    ...

--- A2Solution/shellmemory.h
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-void mem_init();
-char *mem_get_value(char *var);
-char* mem_get_value_by_line(int line);
-void mem_set_value(char *var, char *value);
-void clean_mem(int start, int end);
-int add_file_to_mem(FILE* fp, int* pStart, int* pEnd, char* fileID);
